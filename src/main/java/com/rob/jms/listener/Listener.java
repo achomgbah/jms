c38543c6aac43e2ef6e71ea641645ed047ff2992
robinson <mgbahacho@aol.com>
2020-01-24 08:07:01 -0400

    Jms demo for springboot

--- src/main/java/com/rob/jms/listener/Listener.java
+++ src/main/java/com/rob/jms/listener/Listener.java
@@ -2,21 +2,39 @@ package com.rob.jms.listener;
 
 import com.rob.jms.config.JmsConfig;
 import com.rob.jms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 @Slf4j
 @Component
 public class Listener {
 
+    private final JmsTemplate jmsTemplate;
+
     @JmsListener(destination = JmsConfig.MY_QUEUE)
     public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) {
         log.info("Message recieved:{}", helloWorldMessage);
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RECEIVE_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message message) throws JMSException {
+
+        HelloWorldMessage payload = HelloWorldMessage.builder()
+                .id(UUID.randomUUID()).message("Hello paddy").build();
+
+        log.info("Message recieved:{}", helloWorldMessage);
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payload);
     }
 }
--- src/main/java/com/rob/jms/sender/Sender.java
+++ src/main/java/com/rob/jms/sender/Sender.java
@@ -1,13 +1,20 @@
 package com.rob.jms.sender;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rob.jms.config.JmsConfig;
 import com.rob.jms.model.HelloWorldMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
 import java.util.UUID;
 
 @Slf4j
@@ -16,6 +23,7 @@ import java.util.UUID;
 public class Sender {
 
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
     @Scheduled(fixedRate = 2000)
     public void sendMessage() {
@@ -25,4 +33,24 @@ public class Sender {
         jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
         log.info("Message sent");
     }
+
+    @Scheduled(fixedRate = 2000)
+    public void sendAndReciveMessage() throws JMSException {
+        HelloWorldMessage message = HelloWorldMessage.builder()
+                .id(UUID.randomUUID()).message("Hello buddy").build();
+
+        Message recieved = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RECEIVE_QUEUE, session -> {
+            log.info("sending hello");
+            Message helloMessage = null;
+            try {
+                helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            helloMessage.setStringProperty("_type", HelloWorldMessage.class.getCanonicalName());
+            return helloMessage;
+        });
+        log.info("Message recieved:{}", recieved.getBody(String.class));
+    }
+
 }
